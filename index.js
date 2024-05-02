ce498a2eb088d123c5548629381f00376bae7cb8
unknown <mututadaniel54@gmail.com>
2024-05-02 04:20:22 -0700

    index

---
+++ index.js
@@ -0,0 +1,13 @@
+document.addEventListener('scroll',()=>{
+    const navigation = document.querySelector('.navigation');
+if (window.scrollY > 0) {
+    navigation.classList.add('scrolled');
+}else{
+    navigation.classList.remove('scrolled');
+}
+});
+
+
+
+
+
